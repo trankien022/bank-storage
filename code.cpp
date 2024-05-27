315bfb540e4ba81b5b080d14d5a40a75ed475034
trankien022 <kienjazzmusic@gmail.com>
2024-05-27 20:33:16 +0700

    update code

---
+++ code.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+
+using namespace std;
+// Function to query the balance of a user
+int queryBalance(const string&account){
+    string filePath("./storage/" + account + ".txt");
+    ifstream accountFile(filePath);
+    if (!accountFile.is_open()){
+        return 0;
+    }
+    int balance = 0;
+    accountFile >> balance;
+    accountFile.close();
+    return balance;
+}
+int main() {
+    while (true) {
+        cout << "Main Menu:\n";
+        cout << "1. Query balance\n";
+        cout << "2. Exit\n";
+        cout << "Enter your choice: ";
+        
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            cout << "Enter user account: ";
+            string account;
+            cin >> account;
+            int balance = queryBalance(account);
+            cout << "Balance: " << balance << endl;
+        } else if (choice == 2) {
+            break;
+        } else {
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+    return 0;
+}
